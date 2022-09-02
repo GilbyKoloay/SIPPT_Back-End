f78a667ad1beac65c4ed85b7ff24e9969e1a8f19
GilbyKoloay <lolwtf10001@gmail.com>
2022-09-02 12:09:17 +0800

    Add database connection

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
-app.listen(4000, () => {
-    console.log(`Listening on port 4000.`);
-});
+// connect to database and listen for requests
+mongoose.connect(process.env.DATABASE_URI)
+    .then(() => {
+        console.log(`Database connection successful.`);
+
+        // listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log(`Listening on port ${process.env.PORT}.`);
+        });
+    })
+    .catch((e) => {
+        console.log(`Database connection failed.\n`, e);
+    });
