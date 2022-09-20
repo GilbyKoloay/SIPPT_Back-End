586d8eb46e8864560d0eed48de8b50be1131ab62
GilbyKoloay <lolwtf10001@gmail.com>
2022-09-20 14:33:52 +0800

    add 'get' API for 'Drugs'

---
+++ controllers/Drugs/get.js
@@ -0,0 +1,44 @@
+const db = require('../../models/Drugs');
+const mongoose = require('mongoose');
+
+// get single data in Drugs collections
+module.exports = async (req, res) => {
+    const { _id } = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(400).json({
+            status: `error`,
+            msg: `ID Obat tidak valid`,
+            desc: null,
+            data: null,
+        });
+    }
+
+    try {
+        const result = await db.findOne({ _id });
+
+        if(!result) {
+            return res.status(200).json({
+                status: `error`,
+                msg: `Data Obat tidak ditemukan`,
+                desc: null,
+                data: null,
+            });
+        }
+        
+        res.status(200).json({
+            status: `success`,
+            msg: `Berhasil mengambil data Obat`,
+            desc: null,
+            data: result,
+        });
+    }
+    catch(e) {
+        res.status(500).json({
+            status: `error`,
+            msg: `Gagal mengambil data Obat`,
+            desc: e.message,
+            data: null,
+        });
+    }
+};
--- routes/Drug.js
+++ routes/Drug.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 // importing Drugs controllers
 const getAll = require('../controllers/drugs/getAll');
-// const get = require('../controllers/drugs/get');
+const get = require('../controllers/drugs/get');
 const create = require('../controllers/drugs/create');
 // const change = require('../controllers/drugs/change');
 // const deleteData = require('../controllers/drugs/delete');
@@ -10,7 +10,7 @@ const create = require('../controllers/drugs/create');
 const router = express.Router();
 
 router.get('/getAll', getAll);          // get all drugs data
-// router.get('/get', get);                // get drug data
+router.get('/get', get);                // get drug data
 router.post('/create', create);         // create new drug
 // router.patch('/change', change);        // change drug data
 // router.delete('/delete', deleteData);   // delete drug data
