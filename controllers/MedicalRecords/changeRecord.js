9f2293367c01a8dabb76ae970195af00bfce2a57
GilbyKoloay <lolwtf10001@gmail.com>
2022-10-04 13:45:44 +0800

    modified 'changeRecord' controller in 'MedicalRecords'

--- controllers/MedicalRecords/changeRecord.js
+++ controllers/MedicalRecords/changeRecord.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // change data in Medical Records collections
 module.exports = async (req, res) => {
     const {
+        _employee,
         _id,
         _record,
         bodyHeight,
@@ -13,7 +14,10 @@ module.exports = async (req, res) => {
         respiration,
         bodyTemperature,
         laboratorium,
-        his_phyExam_dia,
+        history,
+        physicalExamination,
+        diagnosis,
+        medicalPrescription,
         suggestion,
         initials,
     } = req.body;
@@ -28,40 +32,78 @@ module.exports = async (req, res) => {
     }
 
     try {
-        const MRresult = await db.findOne({ _id });
-        if(!MRresult) {
+        let MR = await db.findOne({ _id }, { records: 1, changeLog: 1 });
+        // check if medical ecord exists
+        if(!MR) {
             return res.status(404).json({
                 status: "error",
-                msg: `Rekaman di rekam medis tidak ditemukan`,
+                msg: `ID rekam medis tidak ditemukan`,
+                desc: null,
+                data: null,
+            });
+        }
+        // check if record in medical record exist
+        if(!MR.records.find(r => r._id.toString() === _record)) {
+            return res.status(404).json({
+                status: "error",
+                msg: `Catatan di rekam medis tidak ditemukan`,
                 desc: null,
                 data: null,
             });
         }
 
-        let records = MRresult.records;
-        if(records.length !== 0) {
-            records.forEach(r => {
-                if(r._id.toString() === _record) {
-                    r.bodyHeight = bodyHeight;
-                    r.bodyWeight = bodyWeight;
-                    r.tension = tension;
-                    r.pulse = pulse;
-                    r.respiration = respiration;
-                    r.bodyTemperature = bodyTemperature;
-                    r.laboratorium = laboratorium;
-                    r.his_phyExam_dia = his_phyExam_dia;
-                    r.suggestion = suggestion;
-                    r.initials = initials;
-                }
-            });
-        }
+        // let records = MR.records;
+        // if(records.length !== 0) {
+        //     records.forEach(r => {
+        //         if(r._id.toString() === _record) {
+        //             r.bodyHeight = bodyHeight;
+        //             r.bodyWeight = bodyWeight;
+        //             r.tension = tension;
+        //             r.pulse = pulse;
+        //             r.respiration = respiration;
+        //             r.bodyTemperature = bodyTemperature;
+        //             r.laboratorium = laboratorium;
+        //             r.his_phyExam_dia = his_phyExam_dia;
+        //             r.suggestion = suggestion;
+        //             r.initials = initials;
+        //         }
+        //     });
+        // }
+        // const result = await db.updateOne({ _id }, {
+        //     records: records,
+        // });
+        
+        MR.records.map(r => {
+            if(r._id.toString() === _record) {
+                r.bodyHeight = bodyHeight;
+                r.bodyWeight = bodyWeight;
+                r.tension = tension;
+                r.pulse = pulse;
+                r.respiration = respiration;
+                r.bodyTemperature = bodyTemperature;
+                r.laboratorium = laboratorium;
+                r.history = history;
+                r.physicalExamination = physicalExamination;
+                r.diagnosis = diagnosis;
+                r.medicalPrescription = medicalPrescription;
+                r.suggestion = suggestion;
+                r.initials = initials;
+            }
+        });
+        
         const result = await db.updateOne({ _id }, {
-            records: records,
+            $push: { changeLog: {
+                _changedBy: _employee,
+                description: "Mengubah catatan di rekam medis",
+            }},
+            $set: {
+                records: MR.records,
+            },
         });
         
         res.status(201).json({
             status: "success",
-            msg: `Berhasil mengubah rekaman di rekam medis`,
+            msg: `Berhasil mengubah catatan di rekam medis`,
             desc: null,
             data: result,
         });
@@ -69,7 +111,7 @@ module.exports = async (req, res) => {
     catch(e) {
         res.status(500).json({
             status: "error",
-            msg: `Gagal mengubah rekaman di rekam medis`,
+            msg: `Gagal mengubah catatan di rekam medis`,
             desc: e.message,
             data: null,
         });
