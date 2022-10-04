e5840dfe7308870664b7539cdecc116f4baecc1d
GilbyKoloay <lolwtf10001@gmail.com>
2022-10-04 14:39:52 +0800

    modified 'change' controller in 'Patients'

--- controllers/patients/change.js
+++ controllers/patients/change.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 // change data in Patients collections
 module.exports = async (req, res) => {
     const {
+        _employee,
         _id,
         medicalRecordNumber,
         name,
@@ -19,6 +20,17 @@ module.exports = async (req, res) => {
         job,
     } = req.body;
 
+    // check employee's (changedBy) id
+    if(!mongoose.Types.ObjectId.isValid(_employee)) {
+        return res.status(400).json({
+            status: "error",
+            msg: `ID pegawai tidak valid`,
+            desc: null,
+            data: null,
+        });
+    }
+    
+    // check patient's id
     if(!mongoose.Types.ObjectId.isValid(_id)) {
         return res.status(400).json({
             status: "error",
@@ -30,18 +42,24 @@ module.exports = async (req, res) => {
 
     try {
         const result = await db.updateOne({ _id }, {
-            medicalRecordNumber,
-            name,
-            birthPlace,
-            birthDate,
-            sex,
-            familyCardName,
-            address,
-            phoneNumber,
-            paymentMethod,
-            religion,
-            maritalStatus,
-            job,
+            $push: { changeLog: {
+                _changedBy: _employee,
+                description: "Mengubah data pasien",
+            }},
+            $set: {
+                medicalRecordNumber,
+                name,
+                birthPlace,
+                birthDate,
+                sex,
+                familyCardName,
+                address,
+                phoneNumber,
+                paymentMethod,
+                religion,
+                maritalStatus,
+                job,
+            },
         });
         
         res.status(201).json({
